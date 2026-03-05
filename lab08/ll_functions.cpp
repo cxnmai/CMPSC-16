1340ba427acfccd0bab275c09001bd3518070036
Chinmay Khaladkar <cbkhalad@gmail.com>
2026-03-04 22:40:51 -0800

    Update ll_functions.cpp

--- lab08/ll_functions.cpp
+++ lab08/ll_functions.cpp
@@ -1,5 +1,4 @@
 // file: ll_functions.cpp
-#include "ll_headers.h"
 
 // Used in the function createLL()
 // This function is started for you correctly - you have to complete it.
@@ -13,50 +12,105 @@ void h_insert(LinkNodePtr& head, string nom, int num) {
     tmp_ptr->link = nullptr; 
 
 // If the list is empty, the new node becomes the head
-
-
+    if (head == nullptr) {
+        head = tmp_ptr;
+        return;
+    }
 
 // If the list is not empty, traverse to the last node
-
-    
+    LinkNodePtr cur = head;
+    while (cur->link != nullptr) {
+        cur = cur->link;
+    }
 
 // Change the pointer of the last node to the new node
-
-
+    cur->link = tmp_ptr;
 }
 
 void createLL(LinkNodePtr& h) {
-    // Incomplete function - student must complete
     string nom = "";
-    int num;  
-    while(0) // THIS IS A STUB
+    int num = 0;
+    while (true)
     {
         cout << "Enter name, then a number. To quit, enter 0 for the name AND 0 for the number:\n";
-        cin >> nom >> num;
-        // Incomplete code here...
-        // At some point, you call:    h_insert(h, nom, num);
+        if (!(cin >> nom >> num)) {
+            return;
+        }
+        if (nom == "0" && num == 0) {
+            break;
+        }
+        h_insert(h, nom, num);
     }
 }
 
 void printLL(LinkNodePtr h) {
-    // Incomplete function - student must complete
-    cout << "printLL\n"; // THIS IS A STUB
+    LinkNodePtr cur = h;
+    while (cur != nullptr) {
+        cout << cur->name << " " << cur->number << endl;
+        cur = cur->link;
+    }
 }
 
-void insertNodeAfter(LinkNodePtr h) {
-    // Incomplete function - student must complete
-    cout << "insertNodeAfter\n"; // THIS IS A STUB
+void insertNodeAfter(LinkNodePtr& h) {
+    int pos = 0;
+    string nom = "";
+    int num = 0;
+    cout << "Enter position after which to insert (0 inserts at head): ";
+    cin >> pos;
+    cout << "Enter name and number to insert:\n";
+    cin >> nom >> num;
+
+    LinkNodePtr new_node = new LinkNode;
+    new_node->name = nom;
+    new_node->number = num;
+    new_node->link = nullptr;
+
+    if (h == nullptr || pos <= 0) {
+        new_node->link = h;
+        h = new_node;
+        return;
+    }
+
+    LinkNodePtr cur = h;
+    int i = 1;
+    while (cur->link != nullptr && i < pos) {
+        cur = cur->link;
+        i++;
+    }
+    new_node->link = cur->link;
+    cur->link = new_node;
 }
 
 void findMax(LinkNodePtr h) {
-    // Incomplete function - student must complete
-    int max(0);
+    if (h == nullptr) {
+        cout << "Largest number in the list is: 0" << endl;
+        return;
+    }
+
+    int max = h->number;
+    LinkNodePtr cur = h->link;
+    while (cur != nullptr) {
+        if (cur->number > max) {
+            max = cur->number;
+        }
+        cur = cur->link;
+    }
     cout << "Largest number in the list is: " << max << endl;
 }
 
 void findMin(LinkNodePtr h) {
-    // Incomplete function - student must complete
-    int min(0);
+    if (h == nullptr) {
+        cout << "Smallest number in the list is: 0" << endl;
+        return;
+    }
+
+    int min = h->number;
+    LinkNodePtr cur = h->link;
+    while (cur != nullptr) {
+        if (cur->number < min) {
+            min = cur->number;
+        }
+        cur = cur->link;
+    }
     cout << "Smallest number in the list is: " << min << endl;
 }
-
